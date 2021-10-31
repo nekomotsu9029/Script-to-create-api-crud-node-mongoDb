9025e28f2a46b5c72affbfcd1524c644cde3bb21
nekomotsu9029 <gustavo.olmedo@softwareestrategico.com>
2021-10-30 23:49:12 -0500

    optimizacion del codigo

--- README.md
+++ README.md
@@ -1,41 +1,34 @@
 # Como usar?
 
-Copia el script "app.js" y pegalo en la carpeta donde tendras el proyecto, luego en una consola abierta dentro de la carpeta debes configurar el script definiendo el nombre y haciendo un objeto que contenga todas las colecciones que tendra nuestra base de datos MongoDB.
+Copia el script "app.js" y pegalo en la carpeta donde tendras el proyecto, luego en una consola abierta dentro de la carpeta debes configurar el script
 
-*Ejemplo de configuracion:*
+## Configuracion del script
 
-//Nombre de la base de datos
+const settings = {
+    project_s_name: "generateProject",//String
+    database_name: "exampleDb",//String
+    server_port: 3000,//int
+    login: true,//boolean
+    graphql: false,//boolean
+    collections: //obj
+    {
+        "client": {"type": "String", "last_name": "String", "name": "String", "town": "String"},
+        "item": {"album": "String","year": "String", "amount":"String", "price": "String", "artist": "String"}
+    }
+};
 
-let nombreDeLaBaseDeDatos = "Escuela"
-
-//Agregar las colecciones de la base de datos aca, siguiendo la logica de dupla, clave y tipo de dato
-
-let colecciones = {
-    "usuario": {"nombre": "String", "edad": "String", "carrera": "String"},
-    "carrera": {"nombre": "String", "pensum":"[]"}
-}
-
-*fin del ejemplo*
-
-luego en la carpeta se debe abrir en la terminal o cmd, y ejecutar los siguientes comandos:
-
-1. node app.js
-2. npm init -y
-3. npm install express mongoose morgan
-4. node src/server.js
-
-## Esto esta probado para las versiones
-
-"express": "^4.17.1",
-"mongoose": "^6.0.12",
-"morgan": "^1.10.0"
+* project_s_name: es el nombre de la carpeta que almacenara tu proyecto
+* database_name: es el nombre de la base de datos mongoDb que crearas
+* server_port: es el puerto donde quieres que corra el servidor
+* login: es un booleano, si esta en "true", se creara una coleccion adicional llamada "user" y se agregaran los endPoints de logeo y gestion, ademas que las rutas se protegeran usando token
+* graphql: el servidor por defecto se construye como un servidor REST, pero si este campo es "true" se montara como una api de graphql
+* collections: en este objeto se deben agregar las colecciones que tendra nuestra base de datos
 
 ## Reglas
 
-Si una coleccion se llama "usuario" sera tageada en rutas como "_usuario"
+Si una coleccion se llama "tarea" sera tageada en rutas como "_tarea"
 
 ## Cosas que debo hacer
 
-1. Que todo responda un json
-2. Que post, put, delete regresen los datos por el response
-3. Agregar login y que se maneje por token
+1. agregar la funcionalidad de crear servidor graphql
+2. hacer mas usable el endPoint de put, validando cada campo recibido, con el fin de solo enviar los datos a cambiar, y si no se envian permanezcan igual
--- app.js
+++ app.js
@@ -1,14 +1,480 @@
 const fs = require('fs');
 
-let settings = {
+//edit just this Json :)
+const settings = {
     project_s_name: "generateProject",//String
-    database_name: "wobleDb",//String
+    database_name: "exampleDb",//String
     server_port: 3000,//int
     login: true,//boolean
+    graphql: false,//boolean
     collections: //obj
     {
-        "cliente": {"tipo": "String", "apellido": "String", "nombre": "String", "ciudad": "String"},
-        "item": {"album": "String","año": "String", "cantidad":"String", "precio": "String", "artista": "String"}
+        "client": {"type": "String", "last_name": "String", "name": "String", "town": "String"},
+        "item": {"album": "String","year": "String", "amount":"String", "price": "String", "artist": "String"}
     }
 };
 
+const vector_collections_names = Object.keys(settings.collections);
+const total_number_of_collections = vector_collections_names.length;
+const get_the_value_of_the_collection_by_position = (position) => settings.collections[vector_collections_names[position]];
+
+const generate_folder_structure = ()=>{
+    try{
+        fs.mkdirSync(`./${settings.project_s_name}/src/database/`,{recursive:true});
+        fs.mkdirSync(`./${settings.project_s_name}/src/models/`,{recursive:true});
+        fs.mkdirSync(`./${settings.project_s_name}/src/public/`,{recursive:true});
+        fs.mkdirSync(`./${settings.project_s_name}/src/routes/`,{recursive:true});
+    }catch(e){
+        console.log('Err in generate_folder_structure | '+e)
+    }
+}
+
+const create_main_file = ()=>{
+    let content = `//requiero mis dependencias de desarrollo
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+//inicializo express y la base de datos
+const app = express();
+require('./database/database.js');
+
+//configuro el puerto
+app.set('port', process.env.PORT || ${settings.server_port});
+
+//defino la carpeta publica que enviara el servidor al cliente
+app.use(express.static( path.join( __dirname, 'public' ) ));
+
+//configuro los middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+app.use(require('./routes/routes.js'));
+
+app.listen(app.get('port'), ()=>{
+    console.log('el servidor esta a la escucha de peticiones en el puerto', app.get('port'));
+})`
+    try{
+        fs.writeFileSync(`./${settings.project_s_name}/src/server.js`, content)
+    }catch(e){
+        console.log('Err in create_main_file | '+e)
+    }
+}
+
+const create_file_with_database_connection = ()=>{
+    let content = `const mongoose = require('mongoose');
+
+const _url = 'mongodb://localhost:27017/${settings.database_name}';
+
+mongoose.connect(_url, {useUnifiedTopology: true, useNewUrlParser: true})
+    .then(db => console.log('Database is conected :)'))
+    .catch(err => console.error(err));`
+    try{
+        fs.writeFileSync(`./${settings.project_s_name}/src/database/database.js`, content)
+    }catch(e){
+        console.log('Err in create_file_with_database_connection | '+e)
+    }
+}
+
+const get_collection_model_in_position = (position)=>{
+    return `const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const ${vector_collections_names[position]} = new Schema(${JSON.stringify(get_the_value_of_the_collection_by_position(position))});
+
+module.exports = mongoose.model('${vector_collections_names[position]}', ${vector_collections_names[position]});`
+}
+const create_files_with_collection_schemas = ()=>{
+    let re = /"/g;
+    let content;
+    if(settings.login){
+        content = `const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const {Schema} = mongoose;
+
+const user = new Schema({name: String,email: String,password: String});
+
+user.methods.encryptPassword = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+user.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('user', user);`
+        try{
+            fs.writeFileSync(`./${settings.project_s_name}/src/models/user.js`, content);
+        }catch(e){
+            console.log('Err in create_files_with_collection_schemas, user | '+e)
+        }
+    
+    }
+    for(let i=0; i<total_number_of_collections; i++){
+        content = get_collection_model_in_position(i).replace(re, '');
+        try{
+            fs.writeFileSync(`./${settings.project_s_name}/src/models/${vector_collections_names[i]}.js`, content)
+        }catch(e){
+            console.log('Err in create_files_with_collection_schemas, i='+i+' | '+e)
+        }
+    }
+}
+
+const get_template_import_models = ()=>{
+    let content;
+    if(settings.login){
+        content += `
+const jwt = require('jsonwebtoken')
+const secretToken = 'exampleSecretToken'
+
+const _user = require('../models/user');
+`
+    }
+    for(let i=0; i<total_number_of_collections; i++){
+        content += `
+const _${vector_collections_names[i]} = require('../models/${vector_collections_names[i]}');
+`
+    }
+    return content;
+}
+const get_method_get_from_collection = (position)=>{
+    if(settings.login){
+        return `router.get('/api/${vector_collections_names[position]}', async (req, res)=>{
+    const tokenClient = req.header('x-access-token');
+    if(!tokenClient){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(tokenClient, secretToken);
+    const user = await _user.findById(decoded.id, {password: 0});
+    if(!user){
+        return res.json({
+            auth: false,
+            message: 'No user found'
+        })
+    }
+    //si llega aqui es por que encontro el usuario gracias al token
+    const _${vector_collections_names[position]}Find = await _${vector_collections_names[position]}.find();
+    res.json({
+        ${vector_collections_names[position]}s: _${vector_collections_names[position]}Find
+    });
+});`
+    }else{
+        return `
+router.get('/api/${vector_collections_names[position]}', async (req, res)=>{
+    const _${vector_collections_names[position]}Find = await _${vector_collections_names[position]}.find();
+    res.json({
+        ${vector_collections_names[position]}s: _${vector_collections_names[position]}Find
+    });
+});`
+    }
+    
+}
+const get_method_post_from_collection = (position)=>{
+    if(settings.login){
+        return `router.post('/api/${vector_collections_names[position]}', async (req, res)=>{
+    const tokenClient = req.header('x-access-token');
+    if(!tokenClient){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(tokenClient, secretToken);
+    const user = await _user.findById(decoded.id, {password: 0});
+    if(!user){
+        return res.json({
+            auth: false,
+            message: 'No user found'
+        })
+    }
+    //si llega aqui es por que encontro el usuario gracias al token
+    const _${vector_collections_names[position]}Save = new _${vector_collections_names[position]}(req.body);
+    await _${vector_collections_names[position]}Save.save();
+    res.json('Se creo el documento en la coleccion ${vector_collections_names[position]}')
+});`
+    }else{
+        return `router.post('/api/${vector_collections_names[position]}', async (req, res)=>{
+    const _${vector_collections_names[position]}Save = new _${vector_collections_names[position]}(req.body);
+    await _${vector_collections_names[position]}Save.save();
+    const _${vector_collections_names[position]}Find = await _${vector_collections_names[position]}.find();
+    res.json({
+        ${vector_collections_names[position]}s: _${vector_collections_names[position]}Find
+    });
+});`
+    }
+    
+}
+const get_method_put_from_collection = (position)=>{
+    if(settings.login){
+        return `router.put('/api/${vector_collections_names[position]}/:id', async (req, res)=>{
+    const tokenClient = req.header('x-access-token');
+    if(!tokenClient){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(tokenClient, secretToken);
+    const user = await _user.findById(decoded.id, {password: 0});
+    if(!user){
+        return res.json({
+            auth: false,
+            message: 'No user found'
+        })
+    }
+    //si llega aqui es por que encontro el usuario gracias al token
+    const {id} = req.params;
+    await _${vector_collections_names[position]}.update({_id: id}, req.body);
+    res.json('Se actualizo el documento de la coleccion ${vector_collections_names[position]} con id: '+id)
+});`
+    }else{
+        return `router.put('/api/${vector_collections_names[position]}/:id', async (req, res)=>{
+    const {id} = req.params;
+    await _${vector_collections_names[position]}.update({_id: id}, req.body);
+    const _${vector_collections_names[position]}Find = await _${vector_collections_names[position]}.find();
+    res.json({
+        ${vector_collections_names[position]}s: _${vector_collections_names[position]}Find
+    });
+});`
+    }
+    
+}
+const get_method_delete_from_collection = (position)=>{
+    if(settings.login){
+        return `router.delete('/api/${vector_collections_names[position]}/:id', async (req, res)=>{
+    const tokenClient = req.header('x-access-token');
+    if(!tokenClient){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(tokenClient, secretToken);
+    const user = await _user.findById(decoded.id, {password: 0});
+    if(!user){
+        return res.json({
+            auth: false,
+            message: 'No user found'
+        })
+    }
+    //si llega aqui es por que encontro el usuario gracias al token
+    const {id} = req.params;
+    await _${vector_collections_names[position]}.remove({_id: id});
+    res.json('Se elimino el documento de la coleccion ${vector_collections_names[position]} con id: '+id)
+});`
+    }else{
+        return `router.delete('/api/${vector_collections_names[position]}/:id', async (req, res)=>{
+    const {id} = req.params;
+    await _${vector_collections_names[position]}.remove({_id: id});
+    const _${vector_collections_names[position]}Find = await _${vector_collections_names[position]}.find();
+    res.json({
+        ${vector_collections_names[position]}s: _${vector_collections_names[position]}Find
+    });
+});`
+    }
+    
+}
+const obtain_end_points = ()=>{
+    let content;
+    if(settings.login){
+        content += `//const {name, email, password} = req.body;
+router.post('/api/signup', async (req, res)=>{
+    const {name, email, password} = req.body;
+    const user = await _user.findOne({email: email});
+    if(user){
+        return res.json({
+            auth: false,
+            message: "Parece que tu Doppelgänger ya a creado una cuenta con este correo :( que miedo!   "
+        })
+    }
+    const userSave = new _user({
+        name,
+        email,
+        password
+    });
+    userSave.password = await userSave.encryptPassword(userSave.password);
+    await userSave.save();
+    const token = jwt.sign({id: userSave._id}, secretToken, {
+        expiresIn: 60 * 60 * 24 * 2
+    })
+    res.json({
+        auth: true,
+        token
+    })
+});
+
+//const {email, password} = req.body;
+router.post('/api/signin', async (req, res)=>{
+    const {email, password} = req.body;
+    const user = await _user.findOne({
+        email
+    });
+    if(!user){
+        return res.json({
+            auth: false,
+            message: "No existe una cuenta Woble con este correo :(   "
+        })
+    }
+    const passwordIsValid = await user.comparePassword(password);
+    if(!passwordIsValid){
+        return res.json({
+            auth: false,
+            message: "Esta contraseña no coincide con la cuenta "+email+'   '
+        })
+    }
+    const token = jwt.sign({id: user._id}, secretToken, {
+        expiresIn: 60 * 60 * 24 * 2
+    })
+    res.json({
+        auth: true,
+        token
+    })
+});
+
+router.get('/api/user', async (req, res)=>{
+    const tokenClient = req.header('x-access-token');
+    if(!tokenClient){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(tokenClient, secretToken);
+    const user = await _user.findById(decoded.id, {password: 0});
+    if(!user){
+        return res.json({
+            auth: false,
+            message: 'No user found'
+        })
+    }
+    res.json({
+        auth: true,
+        user
+    })
+});
+
+//const {name, email, changepass, password} = req.body;
+router.put('/api/user', async (req, res)=>{
+    const tokenClient = req.header('x-access-token');
+    if(!tokenClient){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(tokenClient, secretToken);
+    const user = await _user.findById(decoded.id);
+    if(!user){
+        return res.json({
+            auth: false,
+            message: 'No user found'
+        })
+    }
+    //si llega aqui es por que encontro el usuario gracias al token
+    //ahora procedo a editar el usuario
+    const {name, email, changepass, password} = req.body;
+    user.name = name;
+    user.email = email;
+    if(changepass == true && password != ''){
+        user.password = user.encryptPassword(password);
+    }
+    await _user.updateOne({_id: user._id}, user);
+    res.json({
+        auth: true,
+        user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        }
+    })
+});
+
+router.delete('/api/user', async (req, res)=>{
+    const tokenClient = req.header('x-access-token');
+    if(!tokenClient){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(tokenClient, secretToken);
+    const user = await _user.findById(decoded.id);
+    if(!user){
+        return res.json({
+            auth: false,
+            message: 'No user found'
+        })
+    }
+    //si llega aqui es por que encontro el usuario gracias al token
+    //ahora procedo a eliminar el usuario
+    await _user.remove({_id: user._id});
+    res.json({
+        auth: false,
+        message: 'User delete'
+    })
+});`
+    }
+    for(let i=0; i<total_number_of_collections; i++){
+        //por cada modelo hacemos sus get;post;put;delete
+        content += `
+` + get_method_get_from_collection(i) + `
+
+` + get_method_post_from_collection(i) + `
+
+` + get_method_put_from_collection(i) + `
+
+` + get_method_delete_from_collection(i) + ``
+    }
+    return content;
+}
+const create_routes_file = ()=>{
+    let content = `const express = require('express');
+const router = express.Router();
+${get_template_import_models()}
+${obtain_end_points()}
+
+module.exports = router;`;
+
+    let re = /undefined/g;
+    content = content.replace(re, '')
+
+    try{
+        fs.writeFileSync(`./${settings.project_s_name}/src/routes/routes.js`, content);
+    }catch(e){
+        console.log('Err in create_routes_file | '+e)
+    }
+}
+
+const create_readme_file = ()=>{
+    let additional_modules;
+    if(settings.login){additional_modules += `bcrypt-nodejs jsonwebtoken`}
+    if(settings.graphql){additional_modules += `express-graphql graphql`}
+
+    let content = `Abre una terminar el la carpeta del proyecto y ejecuta paso a paso los siguientes comandos:
+
+1. npm init -y
+2. npm install express mongoose morgan cors ${additional_modules}
+3. node src/server.js`
+    let re = /undefined/g;
+    content = content.replace(re, '')
+    try{
+        fs.writeFileSync(`./${settings.project_s_name}/README.md`, content)
+    }catch(e){
+        console.log('Err in create_readme_file | '+e)
+    }
+}
+
+generate_folder_structure();
+create_main_file();
+create_file_with_database_connection();
+create_files_with_collection_schemas();
+create_routes_file();
+create_readme_file();
